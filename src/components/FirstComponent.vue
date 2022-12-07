88897948480fba30484ec6e1c3ce89e548fac9e5
wander_bicalho <wander.bicalho@feedz.com.br>
2022-12-07 16:37:00 -0300

    data in component

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,9 +1,18 @@
 <template>
-    <h1>Hello Vue</h1>
+    <div>
+        <h1>Hello Vue</h1>
+        <p>Meu nome é {{ name }} e trabalho como {{ job }}</p>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'FirstComponent',
+    data() {
+        return {
+            name: "Wander",
+            job: "Developer"
+        }
+    }
 }
 </script>
