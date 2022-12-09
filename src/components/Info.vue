9920a5433cb37a4a3d956c65a5f37efca0b56363
wander_bicalho <wander.bicalho@feedz.com.br>
2022-12-09 16:58:14 -0300

    dinamic attributes

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -8,17 +8,25 @@
             <li>GO</li>
         </ul>
         <p v-show="showEmail">Email é: teste@test.com</p>
+        <p>Para acessar meu portfólio <a v-bind:href="site" target="_blank">clique aqui</a></p>
+        <Picture />
     </div>
     
 </template>
 
 <script>
+import Picture from "./Picture";
+
 export default {
     name: 'Info',
+    components: {
+        Picture
+    },
     data() {
         return {
             isWorking: true,
-            showEmail: true
+            showEmail: true,
+            site: "https://www.google.com"
         }
     }
 }
---
+++ src/components/Picture.vue
@@ -0,0 +1,16 @@
+<template>
+    <img :src="avatar" />
+</template>
+
+<script>
+import Info from "./Info";
+
+export default {
+    name: 'Picture',
+    data() {
+        return {
+            avatar: '/img/logo.png'
+        }
+    }
+}
+</script>
