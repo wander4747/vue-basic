1a3d3e1028948b444ceec10e94903c54656ae884
wander_bicalho <wander.bicalho@feedz.com.br>
2022-12-09 16:44:22 -0300

    directive if,else and show

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
+        <p v-if="isWorking">Está trabalhando</p>
+        <p v-else>Não está trabalhando</p>
         <p>Tecnologias</p>
         <ul>
             <li>PHP</li>
             <li>GO</li>
         </ul>
+        <p v-show="showEmail">Email é: teste@test.com</p>
     </div>
     
 </template>
@@ -12,5 +15,11 @@
 <script>
 export default {
     name: 'Info',
+    data() {
+        return {
+            isWorking: true,
+            showEmail: true
+        }
+    }
 }
 </script>
